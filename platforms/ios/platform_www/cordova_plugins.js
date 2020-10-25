7ca50561d3f7a39d58f5f5ac40ef8e49df48fcd2
kizunanocoin <kizunanocoin@gmail.com>
2020-10-26 04:06:43 +0700

    change app name

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -10,7 +10,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-android-permissions": "1.1.0"
+    "cordova-plugin-android-permissions": "1.1.0",
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
